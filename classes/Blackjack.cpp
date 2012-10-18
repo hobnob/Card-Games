562312118a2ed9e087b1026d41e31bd490ef2308
David North <bigchimp2005+bitbucket@gmail.com>
2012-10-18 22:01:30 +0100

    Adds dealing
    Adds dealing to the game, as well as game statuses, so that keeping track of where the game is is now easier.

--- classes/Blackjack.cpp
+++ classes/Blackjack.cpp
@@ -1,51 +1,185 @@
 
 #include "Blackjack.h"
+#include "Hand.h"
 
 #include <iostream>
-#include <string>
 
 Blackjack::Blackjack()
 {
     this->_exit    = false;
+    this->_currentPlayer = 0;
 }
 
-void Blackjack::initialise(){}
+void Blackjack::initialise()
+{
+    this->_gameStage = Blackjack::INIT;
+}
 
 void Blackjack::getInput()
 {
-    if ( !(this->_players.size()) )
+    switch ( this->_gameStage )
     {
-        int players;
-        std::cout << "Please enter the number of players: ";
-        std::cin >> players;
+        case Blackjack::INIT:
+            int players;
+            std::cout << "Please enter the number of players: ";
+            std::cin >> players;
 
-        if ( players )
-        {
-            this->_players.reserve( players );
-            for ( int i = 0; i < players; i++ )
+            if ( players )
             {
-                std::string name = "";
+                this->_players.reserve( players );
+                for ( int i = 0; i < players; i++ )
+                {
+                    std::string name = "";
 
-                std::cout << "Please Enter Player " << ( i + 1 ) << "'s Name: ";
-                std::cin >> name;
+                    std::cout << "Please Enter Player ";
+                    std::cout << ( i + 1 ) << "'s Name: ";
+                    std::cin >> name;
 
-                this->_players.push_back(new Player(name));
+                    this->_players.push_back(new Player(name));
+                }
             }
-        }
-        else
-        {
-            std::cin.clear();
-            std::cin.ignore(256, '\n');
-            std::cout << "\nYou have not entered a valid number!\n";
-        }
+            else
+            {
+                std::cin.clear();
+                std::cin.ignore(256, '\n');
+                std::cout << "\nYou have not entered a valid number!\n";
+            }
+
+            break;
     }
-    else
+}
+
+void Blackjack::update()
+{
+    switch ( this->_gameStage )
     {
+        case Blackjack::INIT:
+            if ( this->_players.size() )
+            {
+                this->_gameStage = Blackjack::DEAL;
+            }
+
+            break;
+
+        case Blackjack::DEAL:
+            for ( int i = 0; i < 2; i++ )
+            {
+                for ( int p = 0; p < this->_players.size(); p++ )
+                {
+                    this->_dealer.deal( &this->_players[p]->getHands()[0] );
+                }
+
+                this->_dealer.deal( &this->_dealer.getHands()[0] );
+            }
+
+            this->_outputDealerCard(0);
+            this->_gameStage = Blackjack::BET;
+            break;
+    }
+}
+
+void Blackjack::_outputDealerCard( int cardIndex )
+{
+    Hand* dealerHand = &this->_dealer.getHands()[0];
+    if ( cardIndex < dealerHand->cards.size() )
+    {
+        Card c = dealerHand->cards[cardIndex];
+
+        std::cout << "\nThe dealer's card is " << this->_outputCardValue(c);
+        std::cout << " of " << this->_outputCardSuit(c) << "\n";
 
     }
 }
 
-void Blackjack::update(){}
+std::string Blackjack::_outputCardValue( Card c )
+{
+    std::string val = "";
+
+    switch ( c.value )
+    {
+        case Card::ACE:
+            val = "Ace";
+            break;
+
+        case Card::TWO:
+            val = "Two";
+            break;
+
+        case Card::THREE:
+            val = "Three";
+            break;
+
+        case Card::FOUR:
+            val = "Four";
+            break;
+
+        case Card::FIVE:
+            val = "Five";
+            break;
+
+        case Card::SIX:
+            val = "Six";
+            break;
+
+        case Card::SEVEN:
+            val = "Seven";
+            break;
+
+        case Card::EIGHT:
+            val = "Eight";
+            break;
+
+        case Card::NINE:
+            val = "Nine";
+            break;
+
+        case Card::TEN:
+            val = "Ten";
+            break;
+
+        case Card::JACK:
+            val = "Jack";
+            break;
+
+        case Card::QUEEN:
+            val = "Queen";
+            break;
+
+        case Card::KING:
+            val = "King";
+            break;
+    }
+
+    return val;
+}
+
+std::string Blackjack::_outputCardSuit( Card c )
+{
+    std::string val = "";
+
+    switch ( c.suit )
+    {
+        case Card::CLUBS:
+            val = "Clubs";
+            break;
+
+        case Card::SPADES:
+            val = "Spades";
+            break;
+
+        case Card::HEARTS:
+            val = "Hearts";
+            break;
+
+        case Card::DIAMONDS:
+            val = "Diamonds";
+            break;
+    }
+
+    return val;
+}
+
+
 void Blackjack::draw(){}
 void Blackjack::playSounds(){}
 void Blackjack::clean(){}
--- classes/Blackjack.h
+++ classes/Blackjack.h
@@ -2,15 +2,27 @@
     #define CardGame_Blackjack_h
 
     #include <vector>
+    #include <string>
+    #include "Card.h"
     #include "Dealer.h"
     #include "Player.h"
 
     class Blackjack
     {
         private:
+            enum e_stages {INIT, DEAL, BET, PLAY, PAYOUT, END};
+
+            e_stages _gameStage;
             bool _exit;
+            int _currentPlayer;
             Dealer _dealer;
             std::vector<Player*> _players;
+
+            void _outputDealerCard( int cardIndex );
+            std::string _outputCardValue( Card c );
+            std::string _outputCardSuit( Card c );
+
+
         public:
             Blackjack();
             void initialise();
